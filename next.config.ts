ab0848f4f415a4532d2105549bbdf37c17825d13
Devansh Agarwal <devanshagrawal775@gmail.com>
2026-06-13 22:20:48 +0530

    fix websocket csp

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "connect-src 'self' ws://3.110.108.63:8080 https://onlinecompilerbackend-gnkk.onrender.com;"
+          }
+        ]
+      }
+    ]
+  }
 };
 
 export default nextConfig;
