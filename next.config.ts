a4dbbca672caa7eef43972d36407bc066c438671
Devansh Agarwal <devanshagrawal775@gmail.com>
2026-06-14 10:19:19 +0530

    update cloudflare tunnel url

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "connect-src 'self' wss://idle-pos-endless-invitation.trycloudflare.com https://onlinecompilerbackend-gnkk.onrender.com;"
+            value: "connect-src 'self' wss://idle-pos-endless-invitation.trycloudflare.com https://onlinecompilerbackend-gnkk.onrender.com https://cdn.jsdelivr.net;"
           }
         ]
       }
